44f034061f02f87183d4be48fc8bfb440dd2e48d
queenrashu136-hue <queenrashu136@gmail.com>
2025-12-04 09:16:57 +0530

    Add files via upload

--- events/GroupEvents.js
+++ events/GroupEvents.js
@@ -16,8 +16,8 @@ const getContextInfo = (mentionedJids) => ({
   forwardingScore: 999,
   isForwarded: true,
   forwardedNewsletterMessageInfo: {
-    newsletterJid: '120363406278870899@newsletter',
-    newsletterName: "Qᴜᴇᴇɴ Aᴋᴜᴍᴀ V2",
+    newsletterJid: '120363368882758119@newsletter',
+    newsletterName: "Qᴜᴇᴇɴ Rᴀꜱʜᴜ Mᴅ",
     serverMessageId: 200,
   },
 });
